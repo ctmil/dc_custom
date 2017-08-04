e5b74d58cf48580e93dca257c08db99054bccb94
morrillo <gustavo.orrillo@gmail.com>
2017-08-04 13:26:17 -0300

    Added sale order fields

--- models.py
+++ models.py
@@ -54,3 +54,44 @@ class purchase_order_line(models.Model):
 	desbaste = fields.Float('Desbaste',compute=_compute_desbaste)
 	precio_kg = fields.Float('Precio Kg',compute=_compute_precio_kg)
 	precio_desbaste = fields.Float('Precio c/Desbaste',compute=_compute_precio_desbaste)
+
+class sale_order(models.Model):
+        _inherit = 'sale.order'
+
+	origen = fields.Char('Origen')
+	destino = fields.Char('Destino')
+
+class sale_order_line(models.Model):
+	_inherit = 'sale.order.line'
+
+	@api.multi
+	def _compute_desbaste(self):
+		for sol in self:
+			if sol.kilos_netos > 0:
+				sol.desbaste = ((sol.kilos_netos - sol.kilos_llegada) / sol.kilos_netos) *  100
+
+	@api.multi
+	def _compute_desbaste(self):
+		for sol in self:
+			if sol.kilos_netos > 0:
+				sol.desbaste = ((sol.kilos_netos - sol.kilos_llegada) / sol.kilos_netos) *  100
+
+
+	@api.multi
+	def _compute_precio_kg(self):
+		for sol in self:
+			if sol.kilos_netos > 0:
+				sol.precio_kg = sol.price_unit / sol.kilos_netos 
+
+	@api.multi
+	def _compute_precio_desbaste(self):
+		for sol in self:
+			if sol.kilos_llegada > 0:
+				sol.precio_desbaste = sol.price_unit / sol.kilos_llegada
+
+	kilos_brutos = fields.Float('Kilos Brutos')
+	kilos_netos = fields.Float('Kilos Netos')
+	kilos_llegada = fields.Float('Kilos Llegada')
+	desbaste = fields.Float('Desbaste',compute=_compute_desbaste)
+	precio_kg = fields.Float('Precio Kg',compute=_compute_precio_kg)
+	precio_desbaste = fields.Float('Precio c/Desbaste',compute=_compute_precio_desbaste)
